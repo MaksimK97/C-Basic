67e76fb077d4d31e445d42cab1b13d8f6f1f318e
MaksimK97 <74003505+MaksimK97@users.noreply.github.com>
2020-11-12 17:33:24 +0200

    Updated

--- Program.cs
+++ Program.cs
@@ -1,136 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 class Program
-{
-    struct User
     {
-        public int Age;
-        private void Example()
+        static int GetInt()
         {
+            while (true)
+                if (!int.TryParse(Console.ReadLine(), out int x))
+                    Console.Write("Incorrect entry (numeric value is required).\nTry again: ");
+                else return x;
         }
-    }
-
-    class UserClass
-    {
-        public int Age;
-        private void Example()
+        
+        static int CheckExeptionByInput()
         {
-        }
-    }
+            int result = 0;
+            bool exceptionCatched;
+            do
+            {
+                exceptionCatched = false;
+                try
+                {
+                    result = int.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    exceptionCatched = true;
+                    string message = ex.Message;
+                    Console.WriteLine("Exception occured: " + message);
+                    Console.Write("Try again: ");
+                }
 
-    static void Main()
-    {
-        //Example();
-
-        ReferenceExample();
-        //ComplexExample();
-
-        Console.ReadKey();
-    }
-
-    private static void Example()
-    {
-        User user = new User();
-        user.Age = 17;
-        SetAgeForUser(ref user);
-        Console.WriteLine(user.Age);
-    }
-
-    private static void SetAgeForUser(ref User user)
-    {
-        int t = 5;
-        user.Age = 18;
-        UserClass userClass = new UserClass();
-        userClass.Age = 5;
-
-    }
-
-    private static void ReferenceExample()
-    {
-        Console.WriteLine("-------------");
-        int value = 17;
-        SetInt(value);
-        Console.WriteLine(value);
-
-        Console.WriteLine("-------------");
-        int valueRef = 17; // Обязаны присвоить значение
-        SetIntRef(ref valueRef);
-        Console.WriteLine(valueRef);
-
-        Console.WriteLine("-------------");
-        int valueOut = 17;// Не Обязаны присвоить значение
-        SetIntOut(out valueOut);
-        Console.WriteLine(valueOut);
-
-        Console.WriteLine("-------------");
-        int valueIn = 17;// Обязаны присвоить значение
-        SetIntIn(in valueIn);
-        Console.WriteLine(valueIn);
-
-        Console.WriteLine("-------------");
-        Console.WriteLine("-------------");
-        SetIntOut(out _);
-    }
-
-    private static void ComplexExample()
-    {
-        Complex complex = new Complex();
-        complex.im = 4;
-        complex.re = 4;
-
-        Complex complex2 = new Complex();
-        complex2.im = 2;
-        complex2.re = 2;
-
-        Console.WriteLine(complex.Minus(complex2).ToString());
-    }
-
-    private static void SetInt(int value)
-    {
-        value = 18;
-    }
-
-    private static void SetIntRef(ref int value)
-    {
-        value = 18;// Не Обязаны присвоить значение
-    }
-
-    private static void SetIntOut(out int value)
-    {
-        value = 18;// Обязаны присвоить значение
-    }
-
-    private static void SetIntIn(in int value)
-    {
-        //value = 18;// Не можем присвоить значение
-    }
-
-    class Complex
-    {
-        public double im;
-        public double re;
-
-        public Complex Plus(Complex x2)
-        {
-            Complex result = new Complex();
-
-            result.im = x2.im + im;
-            result.re = x2.re + re;
-
+            } while (exceptionCatched);
             return result;
         }
 
-        public Complex Minus(Complex x2)
+        static void Main(string[] args)
         {
-            Complex x3 = new Complex();
-            x3.im = x2.im - im;
-            x3.re = x2.re - re;
-            return x3;
+            Console.WriteLine("Ahoy! This program counts odd positive numbers.");
+            Console.WriteLine("Enter the numbers one by one, enter 0 to finish: ");
+
+            int AmountOfOddNumbers = 0;
+            while (true)
+            {
+                int number = CheckExeptionByInput();
+                if (number == 0)
+                {
+                    break;
+                }
+                else if (number > 0 && number % 2 == 1)
+                {
+                    AmountOfOddNumbers++;
+                }
+            }
+
+            Console.WriteLine(Environment.NewLine + "Amount of odd positive numbers: " + AmountOfOddNumbers);
+
+            Console.ReadKey();
         }
 
-        public string ToString()
-        {
-            return re + "+" + im + "i";
-        }
-    }
 }
