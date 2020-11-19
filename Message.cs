66ba21bd305c726124796ccec294b9a6d9e27d7b
MaksimK97 <74003505+MaksimK97@users.noreply.github.com>
2020-11-19 17:34:02 +0200

    Homework5

---
+++ Message.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    class Message
+    {
+        static public string text;
+
+        static Message()
+        {
+            text = "So the Light's vaunted justice has finally arrived? Shall I lay down Frostmourne and throw myself at your mercy, Fordring? " +
+                "We'll grant you a swift death, Arthas. More than can be said for the thousands you've tortured and slain. " +
+                "You'll learn of that first hand. When my work is complete, you will beg for mercy -- and I will deny you. Your anguished cries will be testament to my unbridled power. " +
+                "So be it. Champions, attack! ";
+        }
+
+        static public void GetWordsByLength(int len)
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            foreach (string word in words)
+            {
+                if (word == "")
+                    continue;
+                if (word.Length <= len)
+                    Console.Write(word + " ");
+            }
+        }
+
+        static public void DeleteWordByEndChar(char ch)
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            foreach (string word in words)
+            {
+                if (word == "")
+                    continue;
+                if (word[word.Length - 1] == ch)
+                {
+                    Console.Write(word + " ");
+                    text.Replace(word, "");
+                }
+            }
+        }
+
+        static public string FindMaxLengthWord()
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            string maxWord = words[0];
+            int max = words[0].Length;
+
+            foreach (string word in words)
+            {
+                if (word.Length > max)
+                {
+                    max = word.Length;
+                    maxWord = word;
+                }
+            }
+            return maxWord;
+        }
+
+        static public StringBuilder GetLongWordsString()
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            StringBuilder result = new StringBuilder();
+            int max = Message.FindMaxLengthWord().Length;
+            foreach (string word in words)
+            {
+                if (word.Length == max)
+                {
+                    result.Append(word.ToLower() + " ");
+                }
+            }
+            return result;
+        }
+
+        static public void FrequencyAnalysis(string[] words, string text)
+        {
+            Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
+
+            string[] textWords = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            foreach (string word in words)
+            {
+                foreach (string wordInText in textWords)
+                {
+                    if (word == "")
+                        continue;
+                    if (wordInText == word)
+                    {
+                        if (word == "")
+                            continue;
+                        if (wordFrequency.ContainsKey(word))
+                            wordFrequency[word] += 1;
+                        else
+                            wordFrequency.Add(word, 1);
+                    }
+                }
+            }
+            ICollection<string> keys = wordFrequency.Keys;
+
+            String result = String.Format("{0,-10} {1,-10}\n\n", "Word", "Appearance frequency");
+
+            foreach (string key in keys)
+                result += String.Format("{0,-10} {1,-10:N0}\n",
+                                   key, wordFrequency[key]);
+            Console.WriteLine($"\n{result}");
+        }
+    }
+}
